770218ca8a28bfe129252938921c12a0690adcfe
Ariel Lopes <ariellopes888@gmail.com>
2022-06-30 16:11:07 -0300

    Adicionar arquivos de projeto.

---
+++ WPF_Aula19/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using WPF_Aula19.ViewModels;
+
+namespace WPF_Aula19
+{
+   public class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+
+    }
+}
---
+++ WPF_Aula19/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace WPF_Aula19.ViewModels
+{
+    public class MainViewModel:Screen
+    {
+        private string _nomeCompleto;
+
+        public  string NomeCompleto
+        {
+            get { return _nomeCompleto; }
+            set { _nomeCompleto = value; base.NotifyOfPropertyChange(() => NomeCompleto); }
+        }
+
+    }
+}
